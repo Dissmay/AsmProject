365c0e023d89da7bc5ca7aa7338dd3a5adc3adc2
Dissmay <Dissmay21@gmail.com>
2019-10-29 23:54:40 +0200

    5

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,22 @@
 $(function(){
 
  $('.reviews__slide_height').slick({
-  slidesToShow: 2,
+  slidesToShow: 3,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
    responsive: [
     {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+      },      
+    },
+    {
       breakpoint: 500,
       settings: {
         slidesToShow: 1,
-      }
+      },      
     }
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
