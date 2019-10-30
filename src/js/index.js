74aa81e0df553c3c41aeb4baca2316d398411178
Dissmay <Dissmay21@gmail.com>
2019-10-30 11:14:00 +0200

    6

--- src/js/index.js
+++ src/js/index.js
@@ -18,10 +18,26 @@ $(function(){
         slidesToShow: 1,
       },      
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ]
+});
+
+var btnCase = document.querySelector('.cases__button');
+var modalContainer = document.querySelector('.modal-container');
+var closerModalContainer = document.querySelector('.modal-container__closer');
+var modalContainerMain = document.querySelector('.modal-container__main');
+var popap__form = document.querySelector('.popap__form');
+btnCase.addEventListener('click', function(){
+  modalContainer.classList.remove('d-n');
+});
+
+closerModalContainer.addEventListener('click', function(){
+  modalContainer.classList.add('d-n');
+});
+modalContainer.addEventListener('click', function(e){
+  var target = e.target;
+   if(target === modalContainer){
+    modalContainer.classList.add('d-n');
+   }
 });
 
 
